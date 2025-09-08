0b26fc750196eb4b68b6d49fa92d6dada7aaed9d
dev11-intellial <50742644+dev11-intellial@users.noreply.github.com>
2025-09-08 16:12:38 +0530

    Added gitignore and initial project files

---
+++ cream_crunch/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cream_crunch project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cream_crunch.settings')
+
+application = get_asgi_application()
